28e6d6c26f90c15025bf08dc20a1c81e9e05e7a0
Shoma Kato <rai99cos@gmail.com>
2019-09-24 15:31:29 +0900

    Create iOS proj

---
+++ Smiling/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Smiling
+//
+//  Created by Shoma Kato on 2019/09/24.
+//  Copyright © 2019 Shoma Kato. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
